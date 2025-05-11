cdf6baa4451d118840352c45afc8fa6dad9d4255
kev1n999 <sopratesta999@gmail.com>
2025-05-11 13:23:56 -0300

    back

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from gtts import gTTS 
 from pydantic import BaseModel
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class BodyRequest(BaseModel):
     text: str
