679011fc64d93c59a5c0764e5f63964cde60e722
kev1n999 <sopratesta999@gmail.com>
2025-05-12 17:55:25 -0300

    mudando para flask

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,39 +1,30 @@
-import os
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-from gtts import gTTS 
-from pydantic import BaseModel
+import os 
+from flask import Flask, jsonify, request 
+from flask_cors import CORS 
+from gtts import gTTS
 
-app = FastAPI()
+app = Flask(__name__)
+CORS(app)
 
-app.mount("/audio", StaticFiles(directory=os.path.join(os.path.dirname(__file__), "audio")), name="audio")
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],  
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-class BodyRequest(BaseModel):
-    text: str
-    lang: str 
-    
 @app.get("/")
 def home():
-    return {
-        "status": 200,
-    }
+    return "Funcionando!"
+
+@app.route("/converter", methods=["POST"])
+def convert_text_to_voice():
+    data = request.get_json()
+    text = data.get("text")
+    lang = data.get("lang")
     
-@app.post("/convert")
-def convert_text_to_voice(bodyRequest: BodyRequest):
-    try:
-        tts = gTTS(bodyRequest.text, lang=bodyRequest.lang)
-        audio_dir = os.path.join(os.path.dirname(__file__), "audio")
-        
-        tts.save(os.path.join(audio_dir, "audio.mp3"))
-        return { "status": 200 }
-    except Exception as err:
-        print(err)
+    tts = gTTS(text, lang=lang)
+    tts.save("audio.mp3")
+    return jsonify({
+        "success": "true",
+        "data": {
+            "text": text,
+            "lang": lang 
+        }
+    })    
+    
+if __name__ == "__main__":
+    app.run(debug=True)
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,24 +1,16 @@
-import { requestAudio } from "./request.js";
-
 // elements constants
+const textArea = document.getElementById("text");
+const idiomaSelect = document.getElementById("idioma-select");
 const startAudio = document.getElementById("start-audio");
 const downloadAudio = document.getElementById("download-audio");
 
-startAudio.addEventListener("click", async () => {
-    const textArea = document.getElementById("text");
-    const idiomaSelect = document.getElementById("idioma-select");
-
-    if (!textArea.value) return alert("Você precisa inserir um texto!");
-
-    try {
-        await requestAudio(textArea.value, idiomaSelect.value);
-        const audio = new Audio(`http://127.0.0.1:8000/audio/audio.mp3?t=${Date.now()}`);
-        audio.play();
-    } catch (err) {
-        console.log(err);
-    }
-});
-
-downloadAudio.addEventListener("click", async () => {
-    // ...
+startAudio.addEventListener("click", () => {
+    fetch("http://127.0.0.1:50000/converter", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({ text: textArea.value, lang: idiomaSelect.value }) 
+    })
+    .then(r => r.text())
+    .then(r => alert(r))
+    .catch(err => alert(err));
 });
