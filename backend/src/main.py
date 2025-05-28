6af00d9770344efe2458bb8d27b80f54b343e5b1
kev1n999 <sopratesta999@gmail.com>
2025-05-28 15:40:45 -0300

    a

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,30 +1,35 @@
-import os 
-from flask import Flask, jsonify, request 
-from flask_cors import CORS 
-from gtts import gTTS
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from gtts import gTTS 
+from pydantic import BaseModel
 
-app = Flask(__name__)
-CORS(app)
+app = FastAPI()
+app.mount("/", StaticFiles(directory=".", html=True), name="static")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class BodyRequest(BaseModel):
+    text: str
+    lang: str 
+    
 @app.get("/")
 def home():
-    return "Funcionando!"
-
-@app.route("/converter", methods=["POST"])
-def convert_text_to_voice():
-    data = request.get_json()
-    text = data.get("text")
-    lang = data.get("lang")
+    return {
+        "status": 200,
+    }
     
-    tts = gTTS(text, lang=lang)
-    tts.save("audio.mp3")
-    return jsonify({
-        "success": "true",
-        "data": {
-            "text": text,
-            "lang": lang 
-        }
-    })    
-    
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.post("/convert")
+def convert_text_to_voice(bodyRequest: BodyRequest):
+    try:
+        tts = gTTS(bodyRequest.text, lang=bodyRequest.lang)
+        tts.save("audio.mp3")
+        return { "status": 200 }
+    except Exception as err:
+        print(err)
